36488c654d1b77070ff0060432a91003b336240d
Taek Seung Lee <dlxortmd987@gmail.com>
2024-12-02 23:20:35 +0900

    first commit

---
+++ src/test/java/project/side/blogai/BlogAiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.side.blogai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
